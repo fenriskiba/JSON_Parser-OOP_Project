0100e0e7d69905295a978dd5fbf3e40c72ab6a28
fenriskiba <gcarroll2603@gmail.com>
2014-12-03 19:35:20 -0500

    Added explanation for the use of stringstream in comments

--- StringToJsonParser.hpp
+++ StringToJsonParser.hpp
@@ -12,6 +12,9 @@
 #include "JSON_Object_Model/JSON_Object_Model.hpp"
 
 //Set of functions to parse a string (in the form of a stringstream) into a JSON_Value
+//stringstream is used for the easy use of .peek() and .get()
+  //These functions are used primarily for viewing and removing characters through the
+  //recursive layers without adding the complexity of an iterator
 void deleteOneChar(std::stringstream& input);
 
 std::string getName(std::stringstream& input);
